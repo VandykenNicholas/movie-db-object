ccb03ed1d4dac95013e61bae379ab647baa811a8
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-12-16 11:49:20 -0500

    finishing objects

--- JS/objects.js
+++ JS/objects.js
@@ -9,10 +9,35 @@ function Movie(id, title, genre, rating){
 	
 	
 		this.delete = function () {
-		
+			fetch(`https://puffy-thoracic-peanut.glitch.me/movies/`+this.id, {
+				method: `DELETE`
+			}).then(()=> console.log("Deleted:  " + this.id))
+				.then(() => getMovies());
+			
 		}
 		this.add = function(){
-		
+			
+			let raw =
+				{
+					title: this.title,
+					rating: this.rating,
+					genre: this.genre,
+				}
+			;
+			
+			
+		let requestOptions = {
+			method: 'POST',
+				headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(raw),
+		};
+	
+			fetch("https://puffy-thoracic-peanut.glitch.me/movies", requestOptions)
+				.then(()=> console.log("added movie: "+this.title))
+				.then(()=> getMovies());
+			
 		}
 		this.edit = function(){
 		
