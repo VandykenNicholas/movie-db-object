dca65bac44216aac9fad769ea1464bf3167ffdef
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-12-16 11:03:10 -0500

    added for fun

---
+++ JS/objects.js
@@ -0,0 +1,30 @@
+function Movie(id, title, genre, rating){
+		this.id = id
+		// this.poster = poster
+		this.title = title
+		this.genre = genre
+		// this.plot = plot
+		this.rating = rating
+		// this.iRating = irating
+	
+	
+		this.delete = function () {
+		
+		}
+		this.add = function(){
+		
+		}
+		this.edit = function(){
+		
+		}
+		
+		
+		this.thisId = function(){
+			return this.id;
+		}
+}
+
+
+/////      https://puffy-thoracic-peanut.glitch.me/movies
+
+/////      http://www.omdbapi.com/?apikey=91940b5a&t=&plot=short&y=1991
